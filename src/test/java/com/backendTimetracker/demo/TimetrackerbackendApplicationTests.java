92291b11d5f11288e98183c86ee6b46fafcdcfac
Robert Csikos <143592673+csro23ei@users.noreply.github.com>
2024-05-05 10:17:54 +0200

    Add files via upload

---
+++ src/test/java/com/backendTimetracker/demo/TimetrackerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendTimetracker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
